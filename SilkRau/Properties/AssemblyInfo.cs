42eb5edd529626e1e7e0a458a2227a05aad5a339
Vraiment <jemc44@gmail.com>
2020-02-20 08:39:24 -0800

    Update project and assembly configuration

--- SilkRau/Properties/AssemblyInfo.cs
+++ SilkRau/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SilkRau")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility to translate Saffire Advanced Game Engine's SLB files to other formats and vice versa")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Litestone Studios")]
 [assembly: AssemblyProduct("SilkRau")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Litestone 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
