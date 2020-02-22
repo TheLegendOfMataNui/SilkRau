e5f9d0ec9001a1745ec46bd5a8f8ff6878d0f452
Vraiment <jemc44@gmail.com>
2020-02-22 14:24:20 -0800

    Add YamlToSLBConverter class

---
+++ SilkRau.Tests/FileConverters/YamlToSLBConverterTests.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using NSubstitute;
+using NUnit.Framework;
+using SAGESharp.IO.Binary;
+using SilkRau.FileConverters;
+using System;
+using YamlDotNet.Serialization;
+
+namespace SilkRau.Tests.FileConverters
+{
+    class YamlToSLBConverterTests
+    {
+        private readonly IDeserializer yamlDeserializer
+            = Substitute.For<IDeserializer>();
+
+        private readonly IBinarySerializer<object> slbSerializer
+            = Substitute.For<IBinarySerializer<object>>();
+
+        private readonly YamlToSLBConverter.ReadTextFromFile readTextFromFile
+            = Substitute.For<YamlToSLBConverter.ReadTextFromFile>();
+
+        private readonly YamlToSLBConverter.WriteBinaryToFile writeBinaryToFile
+            = Substitute.For<YamlToSLBConverter.WriteBinaryToFile>();
+
+        private readonly IFileConverter fileConverter;
+
+        public YamlToSLBConverterTests()
+        {
+            fileConverter = new YamlToSLBConverter<object>(
+                yamlDeserializer,
+                slbSerializer,
+                readTextFromFile,
+                writeBinaryToFile
+            );
+        }
+
+        [Test]
+        public void Test_Converting_A_File_To_Another()
+        {
+            IBinaryWriter binaryWriter = Substitute.For<IBinaryWriter>();
+            object value = new object();
+            string inputFileName = nameof(inputFileName);
+            string outputFileName = nameof(outputFileName);
+            string contents = nameof(contents);
+
+            readTextFromFile.Invoke(inputFileName).Returns(contents);
+            yamlDeserializer.Deserialize<object>(contents).Returns(value);
+            writeBinaryToFile.Invoke(
+                fileName: Arg.Is(outputFileName),
+                action: Arg.Do<Action<IBinaryWriter>>(action => action(binaryWriter))
+            );
+
+            fileConverter.Convert(inputFilePath: inputFileName, outputFilePath: outputFileName);
+
+            slbSerializer.Received().Write(binaryWriter, value);
+        }
+    }
+}
---
+++ SilkRau/FileConverters/YamlToSLBConverter.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO.Binary;
+using System;
+using YamlDotNet.Serialization;
+
+namespace SilkRau.FileConverters
+{
+    internal static class YamlToSLBConverter
+    {
+        public delegate string ReadTextFromFile(string filePath);
+
+        public delegate void WriteBinaryToFile(string fileName, Action<IBinaryWriter> action);
+    }
+
+    internal sealed class YamlToSLBConverter<T> : IFileConverter
+    {
+        private readonly IDeserializer yamlDeserializer;
+
+        private readonly IBinarySerializer<T> slbSerializer;
+
+        private readonly YamlToSLBConverter.ReadTextFromFile readTextFromFile;
+
+        private readonly YamlToSLBConverter.WriteBinaryToFile writeBinaryToFile;
+
+        public YamlToSLBConverter(
+            IDeserializer yamlDeserializer,
+            IBinarySerializer<T> slbSerializer,
+            YamlToSLBConverter.ReadTextFromFile readTextFromFile,
+            YamlToSLBConverter.WriteBinaryToFile writeBinaryToFile
+        ) {
+            this.yamlDeserializer = yamlDeserializer;
+            this.slbSerializer = slbSerializer;
+            this.readTextFromFile = readTextFromFile;
+            this.writeBinaryToFile = writeBinaryToFile;
+        }
+
+        public void Convert(string inputFilePath, string outputFilePath)
+        {
+            string contents = readTextFromFile(inputFilePath);
+
+            T value = yamlDeserializer.Deserialize<T>(contents);
+
+            writeBinaryToFile(
+                fileName: outputFilePath,
+                action: binaryWriter => slbSerializer.Write(binaryWriter, value)
+            );
+        }
+    }
+}
