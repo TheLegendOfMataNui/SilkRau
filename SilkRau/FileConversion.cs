c85f8ebe6c977ca5e7dbf6b05180e6b644048c89
Vraiment <jemc44@gmail.com>
2020-02-23 13:12:59 -0800

    Add FileConversion class

---
+++ SilkRau/FileConversion.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using Equ;
+using NUtils.MethodBuilders;
+using System;
+using System.Collections.Generic;
+
+namespace SilkRau
+{
+    internal sealed class FileConversion : IEquatable<FileConversion>
+    {
+        private static readonly IEqualityComparer<FileConversion> equalityComparer
+            = MemberwiseEqualityComparer<FileConversion>.ByProperties;
+
+        private static readonly ToStringMethod<FileConversion> toStringMethod
+            = new ToStringMethodBuilder<FileConversion>().UseProperties().Build();
+
+        public FileConversion(FileFormat inputFileFormat, FileFormat outputFileFormat)
+        {
+            InputFileFormat = inputFileFormat;
+            OutputFileFormat = outputFileFormat;
+        }
+
+        public FileFormat InputFileFormat { get; }
+
+        public FileFormat OutputFileFormat { get; }
+
+        public bool Equals(FileConversion other) => equalityComparer.Equals(this, other);
+
+        public override bool Equals(object obj) => Equals(obj as FileConversion);
+
+        public override int GetHashCode() => equalityComparer.GetHashCode(this);
+
+        public override string ToString() => toStringMethod(this);
+    }
+}
