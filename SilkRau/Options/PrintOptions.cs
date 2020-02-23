af27c848c663286b9a6c5e9800f98865a09e0c7a
Vraiment <jemc44@gmail.com>
2020-02-23 11:10:35 -0800

    Add PrintOptions verb

---
+++ SilkRau/Options/PrintOptions.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using CommandLine;
+
+namespace SilkRau.Options
+{
+    [Verb("print", HelpText = "Prints information about what operations are available.")]
+    internal sealed class PrintOptions
+    {
+        public PrintOptions(bool fileTypes, bool conversions)
+        {
+            FileTypes = fileTypes;
+            Conversions = conversions;
+        }
+
+        [Option('t', "file-types", HelpText = "Prints all the file types supported for conversion.", SetName = nameof(FileTypes), Required = true)]
+        public bool FileTypes { get; }
+
+        [Option('c', "conversions", HelpText = "Prints all the supported conversions.", SetName = nameof(Conversions), Required = true)]
+        public bool Conversions { get; }
+    }
+}
