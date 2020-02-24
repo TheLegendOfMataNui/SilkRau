625053ea5ce689537402fc3e18f2678055a08333
Vraiment <jemc44@gmail.com>
2020-02-23 22:44:07 -0800

    Add CommandLineModule for Ninject

---
+++ SilkRau/Ninject/CommandLineModule.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using CommandLine;
+using Ninject.Modules;
+using System;
+using System.IO;
+
+namespace SilkRau.Ninject
+{
+    internal sealed class CommandLineModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<TextWriter>().ToConstant(Console.Out).InSingletonScope();
+            Bind<Parser>().ToConstant(new Parser(settings =>
+            {
+                settings.CaseSensitive = false;
+                settings.HelpWriter = Console.Out;
+            })).InSingletonScope();
+        }
+    }
+}
