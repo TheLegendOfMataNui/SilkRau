36cf859a3ff4dccb4493cc219aff4865cd9135e9
Vraiment <jemc44@gmail.com>
2020-02-23 22:07:31 -0800

    Add Program.Run(ConvertOptions) method

---
+++ SilkRau.Tests/ProgramTests.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using NSubstitute;
+using NUnit.Framework;
+using SilkRau.Options;
+using System;
+
+namespace SilkRau.Tests
+{
+    class ProgramTests
+    {
+        private readonly IFileTypeRegistry fileTypeRegistry;
+
+        private readonly IFileConverterFactory fileConverterFactory;
+
+        private readonly Program program;
+
+        public ProgramTests()
+        {
+            fileTypeRegistry = Substitute.For<IFileTypeRegistry>();
+            fileConverterFactory = Substitute.For<IFileConverterFactory>();
+            program = new Program(fileTypeRegistry, fileConverterFactory);
+        }
+
+        [Test]
+        public void Test_Running_A_Program_For_Converting_A_File_With_Output()
+        {
+            ConvertOptions options = new ConvertOptions(
+                fileType: "fileType",
+                inputFormat: FileFormat.SLB,
+                inputFile: "inputFile.slb",
+                outputFormat: FileFormat.Yaml,
+                outputFile: "outputFile.yaml"
+            );
+            FileConversion fileConversion = new FileConversion(
+                inputFileFormat: options.InputFormat,
+                outputFileFormat: options.OutputFormat
+            );
+            Type fileType = typeof(string);
+            IFileConverter fileConverter = Substitute.For<IFileConverter>();
+
+            fileTypeRegistry.GetTypeForFileType(options.FileType).Returns(fileType);
+            fileConverterFactory.BuildFileConverter(Arg.Is(fileType), Arg.Is(fileConversion)).Returns(fileConverter);
+
+            program.Run(options);
+
+            fileConverter.Received().Convert(options.InputFile, options.OutputFile);
+        }
+
+        [Test]
+        public void Test_Running_A_Program_For_Converting_A_File_Without_Output()
+        {
+            string fileName = nameof(fileName);
+            ConvertOptions options = new ConvertOptions(
+                fileType: "fileType",
+                inputFormat: FileFormat.SLB,
+                inputFile: $"{fileName}.slb",
+                outputFormat: FileFormat.Yaml,
+                outputFile: null
+            );
+            FileConversion fileConversion = new FileConversion(
+                inputFileFormat: options.InputFormat,
+                outputFileFormat: options.OutputFormat
+            );
+            Type fileType = typeof(string);
+            IFileConverter fileConverter = Substitute.For<IFileConverter>();
+
+            fileTypeRegistry.GetTypeForFileType(options.FileType).Returns(fileType);
+            fileConverterFactory.BuildFileConverter(Arg.Is(fileType), Arg.Is(fileConversion)).Returns(fileConverter);
+
+            program.Run(options);
+
+            fileConverter.Received().Convert(options.InputFile, $"{fileName}.yaml");
+        }
+    }
+}
--- SilkRau/Program.cs
+++ SilkRau/Program.cs
@@ -3,18 +3,63 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+using SilkRau.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SilkRau
 {
-    class Program
+    internal sealed class Program
     {
+        private readonly IFileTypeRegistry fileTypeRegistry;
+
+        private readonly IFileConverterFactory fileConverterFactory;
+
+        public Program(
+            IFileTypeRegistry fileTypeRegistry,
+            IFileConverterFactory fileConverterFactory
+        ) {
+            this.fileTypeRegistry = fileTypeRegistry;
+            this.fileConverterFactory = fileConverterFactory;
+        }
+
         static void Main(string[] args)
         {
+        }
+
+        public void Run(ConvertOptions options)
+        {
+            IFileConverter fileConverter = fileConverterFactory.BuildFileConverter(
+                fileType: fileTypeRegistry.GetTypeForFileType(options.FileType),
+                fileConversion: new FileConversion(
+                    inputFileFormat: options.InputFormat,
+                    outputFileFormat: options.OutputFormat
+                )
+            );
+
+            string outputFile = options.OutputFile ??
+                Path.ChangeExtension(options.InputFile, GetExtensionForFormat(options.OutputFormat));
+
+            fileConverter.Convert(
+                 inputFilePath: options.InputFile,
+                 outputFilePath: outputFile
+            );
+        }
+
+        private static string GetExtensionForFormat(FileFormat fileFormat)
+        {
+            if (FileFormat.SLB == fileFormat)
+            {
+                return "slb";
+            }
+            else if (FileFormat.Yaml == fileFormat)
+            {
+                return "yaml";
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
