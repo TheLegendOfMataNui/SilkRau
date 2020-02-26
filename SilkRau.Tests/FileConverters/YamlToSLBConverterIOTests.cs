b9e7513f360c1aeb1b65bfa8ce0c2730caa7ecd4
Vraiment <jemc44@gmail.com>
2020-02-25 23:09:33 -0800

    Allow YamlToSLBConverter to overwrite files

---
+++ SilkRau.Tests/FileConverters/YamlToSLBConverterIOTests.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using FluentAssertions;
+using NUnit.Framework;
+using SilkRau.FileConverters;
+using System;
+using System.IO;
+using System.Text;
+
+namespace SilkRau.Tests.FileConverters
+{
+    class YamlToSLBConverterIOTests
+    {
+        private readonly YamlToSLBConverter<object>.IIO io;
+
+        public YamlToSLBConverterIOTests()
+        {
+            io = new YamlToSLBConverter<object>.IO();
+        }
+
+        [Test]
+        public void Test_Writing_To_An_Existing_File()
+        {
+            byte[] expected = GetBinaryFileContents();
+            string path = Path.GetTempFileName();
+
+            File.WriteAllBytes(path, Encoding.ASCII.GetBytes("This should be gone"));
+
+            io.WriteBinaryToFile(path, binaryWriter => binaryWriter.WriteBytes(expected));
+
+            byte[] result = File.ReadAllBytes(path);
+
+            result.Should().Equal(expected);
+        }
+
+        [Test]
+        public void Test_Writing_To_An_Non_Existing_File()
+        {
+            byte[] expected = GetBinaryFileContents();
+            string path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+            io.WriteBinaryToFile(path, binaryWriter => binaryWriter.WriteBytes(expected));
+
+            byte[] result = File.ReadAllBytes(path);
+
+            result.Should().Equal(expected);
+        }
+
+        private byte[] GetBinaryFileContents() => Encoding.ASCII.GetBytes(
+            $"Running {GetType().FullName} @ {DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss:ffff")}"
+        );
+    }
+}
